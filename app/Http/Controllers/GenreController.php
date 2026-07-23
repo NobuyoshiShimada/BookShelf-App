74ebc49269538298f96939a059063eb3c83649fc
nob <no.nob.n5.biz@gmail.com>
2026-07-23 16:51:47 +0900

    Genre_CRUD実装完了 #13

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use App\Models\Genre;
+use App\Http\Requests\GenreRequest;
 use Illuminate\Http\Request;
 
 class GenreController extends Controller
@@ -29,44 +30,64 @@ class GenreController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(GenreRequest $request)
     {
-        Genre::create([
-            'name' => $request->name,
+        $validated = $request->validated();
+
+        $genre = Genre::create([
+            'name' => $validated['name'],
         ]);
 
-        return redirect()->route('genres.index');
+        return redirect()->route('genres.index')
+        ->with('success', 'ジャンル「' . $genre->name . '」を新しく登録しました。');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Genre $genre)
     {
-        //
+        $books = $genre->books()->paginate(10);
+
+        return view('genres.show', compact('genre', 'books'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Genre $genre)
     {
-        //
+        return view('genres.edit', compact('genre'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(GenreRequest $request, Genre $genre)
     {
-        //
+        $validated = $request->validated();
+
+        $genre->update([
+            'name' => $validated['name'],
+        ]);
+
+        return redirect()->route('genres.index', $genre)
+        ->with('success', 'ジャンル「' . $genre->name . '」の情報を更新しました。');
+
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Genre $genre)
     {
-        //
+        $genre->books()->sync([]);
+
+        $genre->delete();
+
+        return redirect()->route('genres.index')
+        ->with('success', 'ジャンル「' . $genre->name . '」を削除しました。');
     }
+
 }
